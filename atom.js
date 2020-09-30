c0d557a55b0bc17f7ad0b2c4053a7abb6cf98dd7
Lluís Bosch <holalluis@gmail.com>
2020-09-30 22:49:14 +0200

    update

--- atom.js
+++ atom.js
@@ -1,13 +1,17 @@
+const K=9e4; //constant de coulomb
+const g=0;   //constant de gravetat a la terra
+
 class Atom{
   constructor(x,y,radi){
-    this.radi=radi; //radi
     this.x=x;       //posició inicial
     this.y=y;       //posició inicial
-    this.dx=0;      //velocitat inicial
-    this.dy=0;      //velocitat inicial
-    this.carrega=0; //carrega elèctrica
+    this.radi=radi; //radi
 
-    this.gravetat=0;
+    this.dx=0;       //velocitat inicial
+    this.dy=0;       //velocitat inicial
+    this.massa=10;
+
+    this.carrega=0;  //carrega elèctrica
   }
 
   get energia_cinetica(){
@@ -26,32 +30,50 @@ class Atom{
     let forces=[];
     atoms.forEach(a=>{
       if(this==a) return;
-      let v = new Vector(this.x-a.x, this.y-a.y);
-      let d = v.length;
 
+      //vector de l'atom a a aquest
+      let f = new Vector(this.x-a.x, this.y-a.y);
+
+      //llargada del vector (distància)
+      let d = f.length;
+
+      //sentit de la força
       let sentit = this.carrega * a.carrega; //-1 ò 1
 
-      v.producte_escalar(sentit/Math.pow(d,2));
-      forces.push(v);
+      //normalitza el vector perquè sigui unitari
+      f.normalitza();
 
+      //la distància no pot ser 0
+      let d_efectiva = Math.max(2*this.radi, d);
+
+      //aplica força elèctrica
+      f.producte_escalar(K*sentit/Math.pow(d_efectiva,2));
+
+      forces.push(f);
     });
 
     //força total = suma de forces
-    let x_total = forces.map(v=>v.x).sum();
-    let y_total = forces.map(v=>v.y).sum();
+    let x_total = forces.map(f=>f.x).sum();
+    let y_total = forces.map(f=>f.y).sum();
     let F = new Vector(x_total, y_total);
-    F.normalitza();
 
-    //update velocitat
-    this.dx += F.x/this.radi;
-    this.dy += F.y/this.radi + this.gravetat;
+    //modifica velocitat (v = v0 + at), (a = F/m)
+    this.dx += F.x/this.massa;
+    this.dy += F.y/this.massa + g;
+
+    let v = new Vector(this.dx, this.dy);
+    if(v.length>10){
+      v.normalitza();
+      this.dx=v.x;
+      this.dy=v.y;
+    }
   }
 
   update_posicio(){
-    if(this.x<=this.radi){this.x=this.radi; this.dx*=-1}
-    if(this.y<=this.radi){this.y=this.radi; this.dy*=-1}
-    if(this.x+this.radi>=canvas.width ){this.x=canvas.width-this.radi;  this.dx*=-1}
-    if(this.y+this.radi>=canvas.height){this.y=canvas.height-this.radi; this.dy*=-1}
+    if(this.x<=this.radi){this.x=this.radi+1; this.dx*=-1*0.5}
+    if(this.y<=this.radi){this.y=this.radi+1; this.dy*=-1*0.5}
+    if(this.x+this.radi>=canvas.width ){this.x=canvas.width-this.radi-1;  this.dx*=-1*0.1}
+    if(this.y+this.radi>=canvas.height){this.y=canvas.height-this.radi-1; this.dy*=-1*0.1}
     this.x += this.dx;
     this.y += this.dy;
   }
@@ -59,9 +81,9 @@ class Atom{
   dibuixa(){
     c.beginPath();
     c.fillStyle=(function(carrega){
-      if(carrega==0){return "white"}
-      if(carrega<0 ){return "blue" }
-      if(carrega>0 ){return "white"  }
+      if(carrega==0){return "black"}
+      if(carrega<0 ){return "rgba(0,0,0  ,0.8)"}
+      if(carrega>0 ){return "rgba(0,0,255,0.8)" }
     })(this.carrega);
     c.arc(this.x,this.y,this.radi,0,2*Math.PI);
     c.fill();
