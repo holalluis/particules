0174eaa991c91fbe2c5287f95b7d1469d93b704a
Lluís Bosch <holalluis@gmail.com>
2021-04-14 19:48:47 +0200

    update

--- render.js
+++ render.js
@@ -1,22 +1,17 @@
-let canvas=document.querySelector('canvas'); //agafa element <canvas> del DOM
-let ctx=canvas.getContext('2d'); //agafa objecte "context" del canvas
-
-//set dimensions canvas
-canvas.width=window.innerWidth; //1280; //px
-canvas.height=800; //px
-
 //render loop
-let ticks=0; //frames
-(function animate(){
-  requestAnimationFrame(animate); //loop renderització
+let ticks=0; //frames renderitzats
+(function anima(){
+  requestAnimationFrame(anima); //loop renderització
 
   //clear canvas
   ctx.clearRect(0,0,canvas.width,canvas.height);
 
-  //renderitza cada partícula
-  particules.forEach(p=>p.update());
+  //actualitza estat i redibuixa cada partícula
+  particules.forEach(p=>{
+    p.update();
+  });
 
-  //logs
+  //mostrar logs
   if(false&& ticks%500==0){
     console.info({
       //suma energia cinètica totes les partícules
